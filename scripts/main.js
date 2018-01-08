916a1fc14f8dbaece901303b1114c4f55a198308
Adis Bock <adis.bock@gmail.com>
2018-01-08 08:01:48 -0700

    Added new picture

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,10 @@ let images = [];
 images.push(new Image());
 images[images.length - 1].src = "images/Skull.png";
 images.push(new Image());
+images[images.length - 1].src = "images/Skull.png";
+images.push(new Image());
+images[images.length - 1].src = "images/Skull.png";
+images.push(new Image());
 images[images.length - 1].src = "images/House 1.png";
 images.push(new Image());
 images[images.length - 1].src = "images/House 2.png";
@@ -14,13 +18,20 @@ images.push(new Image());
 images[images.length - 1].src = "images/House 3.png";
 
 images[0].onload = function() {
-    c.drawImage(images[0], 5, 5, 100, 200);
+    c.drawImage(images[1], 5, 5, 100, 200);
 }
 
 images[1].onload = function() {
-    c.drawImage(images[1], 110, 5, 100, 200);
+    c.drawImage(images[1], 110, 799, 100, 200);
 }
 
 images[2].onload = function() {
-    c.drawImage(images[1], 220, 5, 100, 200);
+    c.drawImage(images[2], 421, 220, 100, 200);
+}
+
+images[3].onload = function() {
+    c.drawImage(images[3], 320, 5, 100, 200);
+}
+
+images[4].onload = function() {
 }
